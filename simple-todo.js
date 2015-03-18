ad0c7374990962bbbaf09bb472259114da24234e
seraya <takahser@students.zhaw.ch>
2015-03-18 23:00:14 +0100

    added tasks

--- simple-todo.js
+++ simple-todo.js
@@ -1,38 +1,11 @@
+// simple-todos.js
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-  Session.setDefault('fettheit', "dünn");
-
-  // hello template
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-
-  // mass
-  Template.mass.helpers({
-    fettheit: function () {
-      return Session.get('fettheit');
-    }
-  });
-  Template.mass.events({
-    'click span': function () {
-      // increment the counter when button is clicked
-      Session.set('fettheit', "FETT");
-      console.log(Session.get('fettheit'));
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
   });
 }
