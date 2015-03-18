53a88dc82a61ef0e7dccc4a186a4f02689ec7daf
seraya <takahser@students.zhaw.ch>
2015-03-18 22:01:51 +0100

    added fatability feature

--- simple-todo.js
+++ simple-todo.js
@@ -1,17 +1,32 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  Session.setDefault('fettheit', "dünn");
 
+  // hello template
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
-
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+
+  // mass
+  Template.mass.helpers({
+    fettheit: function () {
+      return Session.get('fettheit');
+    }
+  });
+  Template.mass.events({
+    'click span': function () {
+      // increment the counter when button is clicked
+      Session.set('fettheit', "FETT");
+      console.log(Session.get('fettheit'));
     }
   });
 }
