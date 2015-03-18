b1479f8b3fdc76671dd6492d6cf1ba02d4ec348a
seraya <takahser@students.zhaw.ch>
2015-03-19 00:58:49 +0100

    added persistance layer (mongodb)

--- simple-todo.js
+++ simple-todo.js
@@ -1,11 +1,32 @@
 // simple-todos.js
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      // Show newest tasks first
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
   });
 }
